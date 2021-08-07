a0e213db4756f5e7e2f12aaf83c1fed5eab5a3e7
Sergey <piolersa85@mail.ru>
2021-08-07 12:37:19 +0200

    89

--- Life Cycle Of a View/AppDelegate.swift
+++ Life Cycle Of a View/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Первичная настройка параметров  самого пиложения в целом . Определять интерфейс. Стартовый вью контроллер
+        // если работаем без сториборда
+        // пуш уведомления инициализация 
         return true
     }
 
